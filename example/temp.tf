793b63588c476c12d41748a2c92a759ab4a793d9
Alex Bance <Alex.Bance@justice.gov.uk>
2025-03-11 09:52:12 +0000

    fix: import undeleted rg

---
+++ example/temp.tf
@@ -0,0 +1,4 @@
+import {
+  to       = azurerm_resource_group.this
+  id       = "/subscriptions/e6b5053b-4c38-4475-a835-a025aeb3d8c7/resourceGroups/example-app-insights-rg"
+}
--- tests/terratest/azurerm_app_insights_test.go
+++ tests/terratest/azurerm_app_insights_test.go
@@ -31,10 +31,43 @@ func getOutput(t *testing.T, terraformOptions *terraform.Options) (string, strin
 	return instrumentationKey, connectionString, id, workspaceId
 }
 
-func TestCreateNewLogAnalyticsWorkspace(t *testing.T) {
+//func TestCreateNewLogAnalyticsWorkspace(t *testing.T) {
+//	vars := map[string]interface{}{
+//		"subscription_id":              "8cdb5405-7535-4349-92e9-f52bddc7833a",
+//		"log_analytics_workspace_name": "example-log-analytics-workspace",
+//	}
+//
+//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+//		TerraformDir: "../../example",
+//		Upgrade:      true,
+//		Vars: vars,
+//		NoColor:      true,
+//		PlanFilePath: "./plan.out",
+//	})
+//
+//	terraform.InitAndValidate(t, getValidateOptions(t))
+//	terraform.InitAndPlanAndShowWithStruct(t, terraformOptions)
+//
+//	// Destroy the private endpoint first to avoid errors deleting subnet in use
+//	defer func() {
+//		terraform.Destroy(t, getDestroyPrivateEndpointOptions(t, vars))
+//		terraform.Destroy(t, terraformOptions)
+//	}()
+//
+//	terraform.Apply(t, terraformOptions)
+//
+//	instrumentationKey, connectionString, id, workspaceId := getOutput(t, terraformOptions)
+//
+//	assert.NotEmpty(t, instrumentationKey)
+//	assert.NotEmpty(t, connectionString)
+//	assert.NotEmpty(t, id)
+//	assert.NotEmpty(t, workspaceId)
+//	assert.NotEqual(t, "/subscriptions/e6b5053b-4c38-4475-a835-a025aeb3d8c7/resourceGroups/RG-MDV-LAB-INT-01/providers/Microsoft.OperationalInsights/workspaces/LA-MDV-LAB-INT-WS", workspaceId)
+//}
+
+func TestUseExistingEnvironmentLogAnalyticsWorkspace(t *testing.T) {
 	vars := map[string]interface{}{
-		"subscription_id":              "8cdb5405-7535-4349-92e9-f52bddc7833a",
-		"log_analytics_workspace_name": "example-log-analytics-workspace",
+		"subscription_id": "e6b5053b-4c38-4475-a835-a025aeb3d8c7",
 	}
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -64,34 +97,3 @@ func TestCreateNewLogAnalyticsWorkspace(t *testing.T) {
 	assert.NotEmpty(t, workspaceId)
 	assert.NotEqual(t, "/subscriptions/e6b5053b-4c38-4475-a835-a025aeb3d8c7/resourceGroups/RG-MDV-LAB-INT-01/providers/Microsoft.OperationalInsights/workspaces/LA-MDV-LAB-INT-WS", workspaceId)
 }
-
-//func TestUseExistingEnvironmentLogAnalyticsWorkspace(t *testing.T) {
-//	vars := map[string]interface{}{
-//		"subscription_id": "e6b5053b-4c38-4475-a835-a025aeb3d8c7",
-//	}
-//
-//	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-//		TerraformDir: "../../example",
-//		Upgrade:      true,
-//		Vars: vars,
-//		NoColor:      true,
-//		PlanFilePath: "./plan.out",
-//	})
-//
-//	terraform.InitAndValidate(t, getValidateOptions(t))
-//	terraform.InitAndPlanAndShowWithStruct(t, terraformOptions)
-//
-//	// Destroy the private endpoint first to avoid errors deleting subnet in use
-//	defer terraform.Destroy(t, getDestroyPrivateEndpointOptions(t, vars))
-//	defer terraform.Destroy(t, terraformOptions)
-//
-//	terraform.Apply(t, terraformOptions)
-//
-//	instrumentationKey, connectionString, id, workspaceId := getOutput(t, terraformOptions)
-//
-//	assert.NotEmpty(t, instrumentationKey)
-//	assert.NotEmpty(t, connectionString)
-//	assert.NotEmpty(t, id)
-//	assert.NotEmpty(t, workspaceId)
-//	assert.NotEqual(t, "/subscriptions/e6b5053b-4c38-4475-a835-a025aeb3d8c7/resourceGroups/RG-MDV-LAB-INT-01/providers/Microsoft.OperationalInsights/workspaces/LA-MDV-LAB-INT-WS", workspaceId)
-//}
