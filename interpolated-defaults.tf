31f89f679dc7b27b90450c7a6fe3b345f39b4e98
Alex Bance <Alex.Bance@justice.gov.uk>
2025-03-13 11:11:05 +0000

    fix: tweak local

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -4,7 +4,7 @@ locals {
   mgmt_zone                   = strcontains(local.upper_environment, "PRX") || strcontains(local.upper_environment, "PRP") || strcontains(local.upper_environment, "PRP") ? "MPD" : "MDV"
   environment_law_name        = "LA-${local.mgmt_zone}-${local.upper_environment}-INT-WS"
   environment_law_rg_name     = "RG-${local.mgmt_zone}-${local.upper_environment}-INT-01"
-  ampls_scope_name            = var.existing_private_link_scope_name == null && var.existing_private_link_scope_rg_name == null ? azurerm_monitor_private_link_scope.this[0].name : var.existing_private_link_scope_name
+  ampls_scope_name            = var.existing_private_link_scope_name == null && var.existing_private_link_scope_rg_name == null ? var.ampls_pe_subnet_id != null ? azurerm_monitor_private_link_scope.this[0].name : null : var.existing_private_link_scope_name
   ampls_scope_rg_name         = var.existing_private_link_scope_name == null && var.existing_private_link_scope_rg_name == null ? local.resource_group_name : var.existing_private_link_scope_rg_name
   deploy_private_connectivity = (var.existing_private_link_scope_name != null && var.existing_private_link_scope_rg_name != null) || var.ampls_pe_subnet_id != null
 }
