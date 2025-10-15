040e91950d649ad76654e5378ac6486c0922d59f
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2025-10-15 19:34:10 +0100

    udpate PRD

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -1,6 +1,6 @@
 locals {
   upper_environment          = upper(var.environment)
-  mgmt_zone                  = strcontains(local.upper_environment, "PRX") || strcontains(local.upper_environment, "PRP") || strcontains(local.upper_environment, "PRP") ? "MPD" : "MDV"
+  mgmt_zone                  = strcontains(local.upper_environment, "PRX") || strcontains(local.upper_environment, "PRP") || strcontains(local.upper_environment, "PRD") ? "MPD" : "MDV"
   environment_law_name       = "LA-${local.mgmt_zone}-${strcontains(local.upper_environment, "NFT02") ? "NFT" : local.upper_environment}-INT-WS"
   environment_law_rg_name    = "RG-${local.mgmt_zone}-${strcontains(local.upper_environment, "NFT02") ? "NFT" : local.upper_environment}-INT-01"
   internet_ingestion_enabled = var.private_connectivity == null ? true : var.internet_ingestion_enabled
